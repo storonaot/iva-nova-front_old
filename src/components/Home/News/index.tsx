340e9fc2c9deb2f2f8a610aa90e7300fee548324
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2020-04-04 13:16:15 +0300

    Add slider

--- src/components/Home/News/index.tsx
+++ src/components/Home/News/index.tsx
@@ -1,81 +1,32 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
-import media from 'styled-media-query'
 
 import news from './data'
 
 import { NEWS_URL } from '../../../constants/sources'
 
 import Heading from '../../common/Heading'
-import Container, { Wrapper } from '../../common/Container'
-import Card from '../../common/Card'
+import Container from '../../common/Container'
 import Socials from '../../common/Socials'
 import AspectRatioImage from '../../common/AspectRatioImage'
 import ShowOn from '../../common/ShowOn'
+import Button from '../../common/Button'
 
-import bgImage from '../../../static/images/bg1.jpg'
+import ArrowIcon from '../../../static/svg/arrow.svg'
 
-import { LINES_COLOR, SECONDARY_FONT } from '../../../theme'
-
-const Root = styled.div`
-  background-image: url(${bgImage});
-  padding: 50px 0 40px 0;
-`
-
-const PreviewList = styled.div`
-  display: flex;
-  margin: 0 -16px;
-  > * {
-    width: 100%;
-    ${media.greaterThan('medium')`
-      width: 33.33%;
-    `}
-  }
-`
-
-const Preview = styled.div<{ isShown?: boolean }>`
-  display: ${({ isShown = false }) => (isShown ? 'block' : 'none')};
-  ${media.greaterThan('medium')`
-    padding-right: 16px;
-    padding-left: 16px;
-    margin-bottom: 38px;
-    padding-bottom: 16px;
-    &:first-child {
-      border-right: 1px solid ${LINES_COLOR};
-    }
-    &:last-child {
-      border-left: 1px solid ${LINES_COLOR};
-    }
-  `}
-`
-
-const ImageWrapper = styled.div`
-  margin-bottom: 24px;
-`
-
-const Date = styled.span`
-  font-family: ${SECONDARY_FONT};
-  font-weight: 600;
-  margin-bottom: 16px;
-  display: inline-block;
-`
-
-const Description = styled.p`
-  line-height: 1.2em;
-`
-
-const BottomBlock = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
-const HintText = styled.h5`
-  font-family: ${SECONDARY_FONT};
-  font-size: 1.5em;
-  text-align: center;
-  margin-right: 32px;
-`
+import {
+  Root,
+  PreviewList,
+  Preview,
+  ImageWrapper,
+  Date,
+  Description,
+  BottomBlock,
+  HintText,
+  LeftArrowWrapper,
+  RightArrowWrapper,
+  Card,
+  Wrapper,
+} from './styles'
 
 interface NewsItem {
   id: number
@@ -94,29 +45,38 @@ const News = () => {
         <Heading title="Свежие новости" btnTitle="все новости" btnHref={NEWS_URL} />
       </Container>
       <Wrapper>
-        <button
-          type="button"
-          onClick={() => {
-            const prewPreview = activePreview === 0 ? 2 : activePreview - 1
-            setActivePreview(prewPreview)
-          }}
-        >
-          Prew
-        </button>
         <Card>
-          <PreviewList>
-            {news.map((item: NewsItem, index) => (
-              <Preview key={item.id} isShown={index === activePreview}>
-                <ImageWrapper>
-                  <AspectRatioImage image={item.image} aspectRatio="4:3" />
-                </ImageWrapper>
-                <Date>
-                  {item.date} {index}
-                </Date>
-                <Description>{item.description}</Description>
-              </Preview>
-            ))}
-          </PreviewList>
+          <div>
+            <LeftArrowWrapper
+              onClick={() => {
+                const previousPreview = activePreview === 0 ? 2 : activePreview - 1
+                setActivePreview(previousPreview)
+              }}
+            >
+              <ArrowIcon />
+            </LeftArrowWrapper>
+            <PreviewList>
+              {news.map((item: NewsItem, index) => (
+                <Preview key={item.id} isShown={index === activePreview}>
+                  <ImageWrapper>
+                    <AspectRatioImage image={item.image} aspectRatio="4:3" />
+                  </ImageWrapper>
+                  <Date>
+                    {item.date} {index}
+                  </Date>
+                  <Description>{item.description}</Description>
+                </Preview>
+              ))}
+            </PreviewList>
+            <RightArrowWrapper
+              onClick={() => {
+                const nextPreview = activePreview === 2 ? 0 : activePreview + 1
+                setActivePreview(nextPreview)
+              }}
+            >
+              <ArrowIcon />
+            </RightArrowWrapper>
+          </div>
           <ShowOn tablet desktop>
             <BottomBlock>
               <HintText>Чтобы быть в курсе, не забывайте подписаться на нас!</HintText>
@@ -124,15 +84,11 @@ const News = () => {
             </BottomBlock>
           </ShowOn>
         </Card>
-        <button
-          type="button"
-          onClick={() => {
-            const nextPreview = activePreview === 2 ? 0 : activePreview + 1
-            setActivePreview(nextPreview)
-          }}
-        >
-          Next
-        </button>
+        <ShowOn mobile>
+          <Button href={NEWS_URL} isBlock>
+            все новости
+          </Button>
+        </ShowOn>
       </Wrapper>
     </Root>
   )
--- src/components/Home/News/styles.ts
+++ src/components/Home/News/styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import media from 'styled-media-query'
 
 import bgImage from '../../../static/images/bg1.jpg'
+import CardComp from '../../common/Card'
+import { Wrapper as WrapperComp } from '../../common/Container'
 
 import {
   LINES_COLOR,
@@ -16,22 +18,40 @@ import {
 export const Root = styled.div`
   background-image: url(${bgImage});
   padding: ${SPACE_6} 0 ${SPACE_5} 0;
+  position: relative;
+`
+
+export const Wrapper = styled(WrapperComp)`
+  padding: 0 ${SPACE_2};
+  ${media.greaterThan('medium')`
+    padding: 0 ${SPACE_4};
+  `}
+  ${media.greaterThan('large')`
+    padding: 0;
+  `}
+`
+
+export const Card = styled(CardComp)`
+  margin-bottom: ${SPACE_3};
 `
 
 export const PreviewList = styled.div`
   display: flex;
-  margin: 0 -${SPACE_2};
   > * {
     width: 100%;
     ${media.greaterThan('medium')`
       width: 33.33%;
     `}
   }
+  ${media.greaterThan('medium')`
+    margin: 0 -${SPACE_2};
+  `}
 `
 
 export const Preview = styled.div<{ isShown?: boolean }>`
   display: ${({ isShown = false }) => (isShown ? 'block' : 'none')};
   ${media.greaterThan('medium')`
+    display: block;
     margin-bottom: ${SPACE_4};
     padding: 0 ${SPACE_2} ${SPACE_2} ${SPACE_2};
     &:first-child {
@@ -41,6 +61,27 @@ export const Preview = styled.div<{ isShown?: boolean }>`
       border-left: 1px solid ${LINES_COLOR};
     }
   `}
+`
+
+const ArrowWrapper = styled.div`
+  opacity: 0.3;
+  top: 50%;
+  transform: translate(-50%);
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+  ${media.greaterThan('medium')`
+    display: none;
+  `}
+`
+
+export const LeftArrowWrapper = styled(ArrowWrapper)`
+  left: ${SPACE_2};
+`
+export const RightArrowWrapper = styled(ArrowWrapper)`
+  transform: scale(-1);
+  right: 0;
 `
 
 export const ImageWrapper = styled.div`
