21ef098397c17ccfb673bbc63fe580686d33e148
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2020-06-20 14:48:12 +0300

    WIP

---
+++ pages/music/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from '../../src/components/Layout'
+
+const MucicPage = () => <Layout>Mucic</Layout>
+
+export default MucicPage
+
+// import React from 'react'
+
+// import Layout from '../../src/components/Layout'
+// import News from '../../src/components/News'
+
+// const NewsPage = () => (
+//   <Layout>
+//     <News />
+//   </Layout>
+// )
+
+// export default NewsPage
---
+++ src/components/Music/index.tsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+// import Title from '../common/Title'
+// import NewsList from './List'
+
+// import Container from '../common/Container'
+// import Pagination from '../common/Pagination'
+// import NewsRoot from '../common/NewsRoot'
+
+// const Schedule = () => (
+//   <NewsRoot>
+//     <Container>
+//       <Title withMargin>Новости</Title>
+//       <NewsList />
+//       <Pagination />
+//     </Container>
+//   </NewsRoot>
+// )
+
+// export default Schedule
