d699ea964ed373ed1dc82f215472210c608b2865
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2021-01-31 22:20:43 +0300

    Тест на sass

---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../src/components/Layout'
+
+const Test = () => (
+  <Layout>
+    <div>Test</div>
+  </Layout>
+)
+
+export default Test
--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { FC } from 'react'
 import Link from 'next/link'
-import styled from 'styled-components'
+import cn from 'classnames'
 
-import { SPACE_3, SECONDARY_FONT, selectCustomSize, X_SMALL_RADIUS } from '../../../theme'
+import s from './styles.module.scss'
 
 interface Props {
   href: string
@@ -11,29 +11,9 @@ interface Props {
   isBlock?: boolean
 }
 
-const Btn = styled.a<{ isGhost?: boolean; isBlock?: boolean }>`
-  background-color: ${({ theme: { colors }, isGhost }) =>
-    isGhost ? 'rgba(255, 255, 255, 0.2)' : colors.grayDark};
-  padding: 0 ${SPACE_3};
-  display: inline-block;
-  font-size: 1.375em;
-  font-family: ${SECONDARY_FONT};
-  font-weight: 400;
-  line-height: calc(${selectCustomSize('buttonHeight')} - 5px);
-  height: ${selectCustomSize('buttonHeight')};
-  border-radius: ${X_SMALL_RADIUS};
-  border: 1px solid ${({ isGhost, theme: { colors } }) => (isGhost ? '#fff' : colors.grayDark)};
-  cursor: pointer;
-  color: #fff;
-  width: ${({ isBlock }) => (isBlock ? '100%' : 'auto')};
-  text-align: center;
-`
-
-const Button = ({ href, children, isGhost = false, isBlock = false }: Props) => (
+const Button: FC<Props> = ({ href, children, isGhost = false, isBlock = false }) => (
   <Link href={href}>
-    <Btn isGhost={isGhost} isBlock={isBlock}>
-      {children}
-    </Btn>
+    <a className={cn(s.button, { [s.isGhost]: isGhost, [s.isBlock]: isBlock })}>{children}</a>
   </Link>
 )
 
