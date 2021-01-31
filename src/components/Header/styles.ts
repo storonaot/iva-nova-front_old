f1c1331b2821267aec5af3a7c70df0a5d84d9221
Veronica Naberezhneva <vnaberezhneva@mail.ru>
2021-01-31 15:05:48 +0300

    Добавлена тень для хэдера

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -11,6 +11,7 @@ export const HeaderRoot = styled.div`
   border-bottom: 1px solid ${LINES_COLOR};
   background-color: #fff;
   z-index: 1;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.04);
 `
 
 export const HeaderInner = styled(Container)<ContainerProps>`
