f86ba616835919f1d3ef8b01311b9c7fbc11cd84
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2021-03-21 00:53:48 +0300

    Фиксы

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
-import { BASE_URL } from '../constants'
+// import { BASE_URL } from '../constants'
 // export const BASE_URL = 'http://159.89.30.102'
+
+export const BASE_URL = 'https://agile-headland-93975.herokuapp.com/'
 
 const instance = axios.create({
   baseURL: BASE_URL,
