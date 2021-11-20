5a7644278abc80c7bef0d88dbcbaff19edb100aa
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2021-11-20 20:14:37 +0300

    WIP

--- pages/photos/[id].tsx
+++ pages/photos/[id].tsx
@@ -11,7 +11,7 @@ import dbx from '../../src/api/dbx'
 interface Props {
   photoAlbum: PhotoAlbumType
   error?: string
-  photos: files.GetThumbnailBatchResultEntrySuccess[] | null
+  photos: files.GetTemporaryLinkResult[] | null
 }
 
 const PhotoAlbumPage: FC<Props> = ({ photoAlbum, error, photos }) => {
@@ -30,40 +30,14 @@ export const getServerSideProps: GetServerSideProps = async context => {
     if (photoAlbum.dropbox_src) {
       const photoFiles = await (await dbx.filesListFolder({ path: `/${photoAlbum.dropbox_src}` }))
         .result
-
-      const filePaths = photoFiles.entries.map(file => ({ path: file.path_display as string }))
-
-      photoLinks = await (await dbx.filesGetThumbnailBatch({ entries: filePaths })).result.entries
+      photoLinks = await Promise.all(
+        photoFiles.entries.map(file => {
+          return dbx.filesGetTemporaryLink({ path: file.path_lower as string })
+        }),
+      ).then(res => {
+        return Object.values(res).map(i => i.result)
+      })
     }
-
-    // if (photoAlbum.dropbox_src) {
-    //   // const photoFiles = await await dbx.filesGetThumbnailBatch({
-    //   //   path: `/${photoAlbum.dropbox_src}`,
-    //   // })
-
-    //   // console.log('photoFiles', photoFiles)
-
-    //   const photoFiles = await (await dbx.filesListFolder({ path: `/${photoAlbum.dropbox_src}` }))
-    //     .result
-    //   photoLinks = await Promise.all(
-    //     photoFiles.entries.map(file => {
-    //       // return dbx.filesGetTemporaryLink({ path: file.path_lower as string })
-
-    //       // return dbx.filesGetThumbnail({ path: file.path_lower as string })
-
-    //       return dbx.filesGetThumbnailBatch({ path: file.path_lower as string })
-    //     }),
-    //   ).then(res => {
-    //     // return res.map(i => i.result)
-
-    //     console.log('res', res)
-    //     console.log('typeof res', typeof res)
-
-    //     return []
-
-    //     // return Object.values(res).map(i => i.result)
-    //   })
-    // }
 
     return {
       props: {
@@ -81,3 +55,86 @@ export const getServerSideProps: GetServerSideProps = async context => {
 }
 
 export default PhotoAlbumPage
+
+// import React, { FC } from 'react'
+// import { GetServerSideProps } from 'next'
+
+// import { files } from 'dropbox'
+// import PhotoAlbum from '../../src/components/PhotoAlbum'
+// import { fetchPhotoAlbum } from '../../src/api'
+// import { PhotoAlbum as PhotoAlbumType } from '../../src/api/types'
+
+// import dbx from '../../src/api/dbx'
+
+// interface Props {
+//   photoAlbum: PhotoAlbumType
+//   error?: string
+//   photos: files.GetThumbnailBatchResultEntrySuccess[] | null
+// }
+
+// const PhotoAlbumPage: FC<Props> = ({ photoAlbum, error, photos }) => {
+//   return error ? <>{error}</> : <PhotoAlbum photos={photos} photoAlbum={photoAlbum} />
+// }
+
+// export const getServerSideProps: GetServerSideProps = async context => {
+//   const { id } = context.query
+//   if (id == null) throw new Error('')
+
+//   try {
+//     const photoAlbum = await fetchPhotoAlbum(id)
+
+//     let photoLinksThumbs = null
+//     const photoLinks = null
+//     let photos = null
+
+//     if (photoAlbum.dropbox_src) {
+//       const photoFiles = await (await dbx.filesListFolder({ path: `/${photoAlbum.dropbox_src}` }))
+//         .result
+
+//       const photoThumbs = await (await dbx.filesListFolder({ path: `/${photoAlbum.dropbox_src}` }))
+//         .result
+
+//       const filePaths = photoThumbs.entries.map(file => ({
+//         path: file.path_display as string,
+//         size: 'w256h256',
+//       }))
+
+//       photoLinksThumbs = await (await dbx.filesGetThumbnailBatch({ entries: filePaths })).result
+//         .entries
+
+//       photoLinks = await Promise.all(
+//         photoFiles.entries.map(file => {
+//           return dbx.filesGetTemporaryLink({ path: file.path_lower as string })
+//         }),
+//       ).then(res => {
+//         return Object.values(res)
+//           .map(i => i.result)
+//           .reduce((acc, current) => {
+//             return { ...acc, [current.metadata.id]: current }
+//           }, {})
+//       })
+
+//       photos = photoLinksThumbs.map((item, index) => {
+//         return {
+//           ...item,
+//           link: photoLinks != null ? photoLinks[item.metadata.id].link : null,
+//         }
+//       })
+//     }
+
+//     return {
+//       props: {
+//         photoAlbum,
+//         photos,
+//       },
+//     }
+//   } catch (error) {
+//     console.error('error', error.message)
+
+//     return {
+//       props: { error: error.message },
+//     }
+//   }
+// }
+
+// export default PhotoAlbumPage
--- src/components/PhotoAlbum/index.tsx
+++ src/components/PhotoAlbum/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useState, useMemo } from 'react'
+import React, { FC, useCallback, useState } from 'react'
 import { files } from 'dropbox'
 import Title from '../common/Title'
 import Link from '../common/Link'
@@ -19,107 +19,195 @@ import ModalContentWrapper from '../common/Modal/ModalContentWrapper'
 
 interface Props {
   photoAlbum: PhotoAlbumType
-  photos: files.GetThumbnailBatchResultEntrySuccess[] | null
+  photos: files.GetTemporaryLinkResult[] | null
 }
 
 const PhotoAlbum: FC<Props> = ({ photos, photoAlbum }) => {
-  // const [currentPhoto, setCurrentPhoto] = useState<files.GetTemporaryLinkResult | null>(null)
-  // const [currentPhotoIndex, setCurrentPhotoIndex] = useState<number | null>(null)
+  const [currentPhoto, setCurrentPhoto] = useState<files.GetTemporaryLinkResult | null>(null)
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState<number | null>(null)
 
-  // const showFull = useCallback(id => {
-  //   if (photos) {
-  //     const targetIndex = photos.findIndex(photo => photo.metadata.id === id)
-  //     const targetPhoto = photos[targetIndex]
+  const showFull = useCallback(id => {
+    if (photos) {
+      const targetIndex = photos.findIndex(photo => photo.metadata.id === id)
+      const targetPhoto = photos[targetIndex]
 
-  //     if (targetPhoto != null) {
-  //       setCurrentPhoto(targetPhoto)
-  //       setCurrentPhotoIndex(targetIndex)
-  //     }
-  //   }
-  // }, [])
-
-  // console.log('photos', photos)
-
-  // `data:image/jpeg;base64, ${file.thumbnail}`
-
-  const thumbnails = useMemo(() => {
-    if (photos == null) return []
-
-    return photos.map((photo, index) => {
-      return {
-        id: index,
-        thumbnail: `data:image/jpeg;base64, ${photo.thumbnail}`,
+      if (targetPhoto != null) {
+        setCurrentPhoto(targetPhoto)
+        setCurrentPhotoIndex(targetIndex)
       }
-    })
-
-    // return photos.entries.map(photo => {
-    //   console.log('photo', photo)
-
-    //   return {
-    //     id: photo.me
-    //     thumbnail: `data:image/jpeg;base64, ${photo.thumbnail}`,
-    //   }
-    // })
+    }
   }, [])
 
   return (
-    <div>
-      {thumbnails.map(thumbnail => (
-        <img key={thumbnail.id} alt="thumbnail.id as string" src={thumbnail.thumbnail} />
-      ))}
-    </div>
+    <SectionRoot bgImage={bgImage} opacity={0.5}>
+      <Container>
+        <MainHeading>
+          <Title isUpCase>{photoAlbum.title}</Title>
+          <Link href={`${PHOTOS_URL}`}>Все альбомы</Link>
+        </MainHeading>
+        {photoAlbum.description && <Description>{photoAlbum.description}</Description>}
+        {photos && (
+          <Grid gridSize={4}>
+            {photos.map(photo => {
+              return (
+                <div
+                  key={photo.metadata.id}
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => {
+                    showFull(photo.metadata.id)
+                  }}
+                  onKeyPress={() => {}}
+                  tabIndex={0}
+                  role="button"
+                >
+                  <AspectRatioImage imageUrl={photo.link} aspectRatio={AspectRatio['1:1']} />
+                </div>
+              )
+            })}
+          </Grid>
+        )}
+      </Container>
+      <Modal isOpened={!!currentPhoto}>
+        <ModalContentWrapper
+          itemList={photos}
+          setCurrentIndex={setCurrentPhotoIndex}
+          setCurrentItem={setCurrentPhoto}
+          currentIndex={currentPhotoIndex}
+        >
+          {currentPhoto && (
+            <img
+              alt="alt"
+              style={{
+                maxWidth: '80vw',
+                maxHeight: '80vh',
+              }}
+              src={currentPhoto.link}
+            />
+          )}
+        </ModalContentWrapper>
+      </Modal>
+    </SectionRoot>
   )
-
-  // return (
-  //   <SectionRoot bgImage={bgImage} opacity={0.5}>
-  //     <Container>
-  //       <MainHeading>
-  //         <Title isUpCase>{photoAlbum.title}</Title>
-  //         <Link href={`${PHOTOS_URL}`}>Все альбомы</Link>
-  //       </MainHeading>
-  //       {photoAlbum.description && <Description>{photoAlbum.description}</Description>}
-  //       {photos && (
-  //         <Grid gridSize={4}>
-  //           {photos.map(photo => {
-  //             return (
-  //               <div
-  //                 key={photo.metadata.id}
-  //                 style={{ cursor: 'pointer' }}
-  //                 onClick={() => {
-  //                   showFull(photo.metadata.id)
-  //                 }}
-  //                 onKeyPress={() => {}}
-  //                 tabIndex={0}
-  //                 role="button"
-  //               >
-  //                 <AspectRatioImage imageUrl={photo.link} aspectRatio={AspectRatio['1:1']} />
-  //               </div>
-  //             )
-  //           })}
-  //         </Grid>
-  //       )}
-  //     </Container>
-  //     <Modal isOpened={!!currentPhoto}>
-  //       <ModalContentWrapper
-  //         itemList={photos}
-  //         setCurrentIndex={setCurrentPhotoIndex}
-  //         setCurrentItem={setCurrentPhoto}
-  //         currentIndex={currentPhotoIndex}
-  //       >
-  //         {currentPhoto && (
-  //           <img
-  //             alt="alt"
-  //             style={{
-  //               maxWidth: '80vw',
-  //               maxHeight: '80vh',
-  //             }}
-  //             src={currentPhoto.link}
-  //           />
-  //         )}
-  //       </ModalContentWrapper>
-  //     </Modal>
-  //   </SectionRoot>
-  // )
 }
 
 export default PhotoAlbum
+
+// import React, { FC, useCallback, useState, useMemo } from 'react'
+// import { files } from 'dropbox'
+// import Title from '../common/Title'
+// import Link from '../common/Link'
+
+// import { PHOTOS_URL } from '../../constants/sources'
+// import Container from '../common/Container'
+// import bgImage from '../../static/images/bg4.png'
+// import SectionRoot from '../common/SectionRoot'
+
+// import { MainHeading, Description, SpinnerWrapper } from './styles'
+// import Grid from '../common/Grid'
+// import { PhotoAlbum as PhotoAlbumType } from '../../api/types'
+
+// import AspectRatioImage, { AspectRatio } from '../common/AspectRatioImage'
+// import Modal from '../common/Modal'
+
+// import ModalContentWrapper from '../common/Modal/ModalContentWrapper'
+// import dbx from '../../api/dbx'
+// import Spinner from './Spinner'
+
+// interface Props {
+//   photoAlbum: PhotoAlbumType
+//   photos: files.GetThumbnailBatchResultEntrySuccess[] | null
+// }
+
+// const PhotoAlbum: FC<Props> = ({ photos, photoAlbum }) => {
+//   const [currentPhoto, setCurrentPhoto] = useState<files.GetTemporaryLinkResult | null>(null)
+//   const [dataIsFetching, setDataIsFetching] = useState(false)
+//   const [imageLoaded, setImageLoaded] = useState(false)
+
+//   console.log('photos', photos)
+
+//   // const [currentPhotoIndex, setCurrentPhotoIndex] = useState<number | null>(null)
+
+//   const showFull = useCallback(
+//     async path => {
+//       setDataIsFetching(true)
+//       setImageLoaded(false)
+
+//       const result = await (await dbx.filesGetTemporaryLink({ path })).result
+
+//       setCurrentPhoto(result)
+//       setDataIsFetching(false)
+//     },
+//     [setDataIsFetching, setImageLoaded, setCurrentPhoto, setDataIsFetching],
+//   )
+
+//   const thumbnails = useMemo(() => {
+//     if (photos == null) return []
+
+//     return photos.map((photo, index) => {
+//       return {
+//         id: index,
+//         src: `data:image/jpeg;base64, ${photo.thumbnail}`,
+//         path: photo.metadata.path_lower,
+//       }
+//     })
+//   }, [photos])
+
+//   return (
+//     <SectionRoot bgImage={bgImage} opacity={0.5}>
+//       <Container>
+//         <MainHeading>
+//           <Title isUpCase>{photoAlbum.title}</Title>
+//           <Link href={`${PHOTOS_URL}`}>Все альбомы</Link>
+//         </MainHeading>
+//         {photoAlbum.description && <Description>{photoAlbum.description}</Description>}
+//         {photos && (
+//           <Grid gridSize={4}>
+//             {thumbnails.map(photo => {
+//               return (
+//                 <div
+//                   key={photo.id}
+//                   style={{ cursor: 'pointer' }}
+//                   onClick={() => {
+//                     showFull(photo.path)
+//                   }}
+//                   onKeyPress={() => {}}
+//                   tabIndex={0}
+//                   role="button"
+//                 >
+//                   <AspectRatioImage imageUrl={photo.src} aspectRatio={AspectRatio['1:1']} />
+//                 </div>
+//               )
+//             })}
+//           </Grid>
+//         )}
+//       </Container>
+//       <Modal isOpened={!!currentPhoto || dataIsFetching}>
+//         <ModalContentWrapper
+//           itemList={[]}
+//           setCurrentIndex={() => {}}
+//           setCurrentItem={setCurrentPhoto}
+//           currentIndex={0}
+//         >
+//           {/* <SpinnerWrapper isHidden={!imageLoaded || dataIsFetching}>
+//             <Spinner />
+//           </SpinnerWrapper> */}
+//           {currentPhoto ? (
+//             <img
+//               alt="alt"
+//               style={{
+//                 maxWidth: '80vw',
+//                 maxHeight: '80vh',
+//               }}
+//               src={currentPhoto.link}
+//               onLoad={() => {
+//                 setImageLoaded(true)
+//               }}
+//             />
+//           ) : null}
+//         </ModalContentWrapper>
+//       </Modal>
+//     </SectionRoot>
+//   )
+// }
+
+// export default PhotoAlbum
